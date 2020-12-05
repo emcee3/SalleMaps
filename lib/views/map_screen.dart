244a47f658e7442fcbe0859e52ca63e5e5e8c24b
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-12-06 00:09:34 +0100

    Added a default marker and a custom pin.

--- lib/views/map_screen.dart
+++ lib/views/map_screen.dart
@@ -11,26 +11,53 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreen extends State<MapScreen> {
+  BitmapDescriptor _pinLocationIcon;
+  Set<Marker> _markers = {};
   Completer<GoogleMapController> _controller = Completer();
+  static final LatLng _defaultPosition =
+      LatLng(41.408746627104975, 2.130265356254578);
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(41.408746627104975, 2.130265356254578),
+    target: _defaultPosition,
     zoom: 20,
   );
 
   static final CameraPosition _kLake = CameraPosition(
-    target: LatLng(41.408746627104975, 2.130265356254578),
+    target: _defaultPosition,
     zoom: 18.5,
   );
+
+  void _getCustomPin() async {
+    _pinLocationIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(size: Size(20.0, 20.0)),
+      'assets/custom_pin.png',
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCustomPin();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        markers: _markers,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+          setState(() {
+            _markers.add(
+              Marker(
+                markerId: MarkerId('default_marker'),
+                position: _defaultPosition,
+                icon: _pinLocationIcon,
+              ),
+            );
+          });
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
