3b1be47604a9e65bc47756be4235186b6a35dab7
nicos.fabro <nicos.fabro@students.salle.url.edu>
2020-12-08 17:43:40 +0100

    #4 Separated logic from map_screen.dart

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../views/map_screen.dart';
 import '../views/list_screen.dart';
+
+import '../view_models/map_view_model.dart';
 
 import '../widgets/home_bottom_nav_bar.dart';
 import '../widgets/home_floating_action_button.dart';
@@ -14,7 +17,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<Widget> _screens = [MapScreen(), ListScreen()];
+  final List<Widget> _screens = [
+    ChangeNotifierProvider(
+      create: (context) => MapViewModel(),
+      child: MapScreen(),
+    ),
+    ListScreen(),
+  ];
   int _currentTabIndex = 0;
 
   void _selectTabIndex(int index) {
@@ -29,10 +38,11 @@ class _HomeScreenState extends State<HomeScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: HomeFloatingActionButton(),
       bottomNavigationBar: HomeBottomNavBar(onTabSelected: _selectTabIndex),
+      // * IndexedStack prevents from loading the map over and over again
       body: IndexedStack(
         index: _currentTabIndex,
         children: _screens,
-      ), // This prevents from loading the map over and over again
+      ),
     );
   }
 }
